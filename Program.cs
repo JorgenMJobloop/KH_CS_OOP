60e2889fdfab7dfa4d3fb085255bbf0e8f78f078
jorgenmjobloop <jorgen@jobloop.no>
2024-09-16 08:34:42 +0200

    inital commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace KH_CS_OOP;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Name = "John Doe";
+        person.Age = 32;
+        List<string> interests = new List<string>(){
+            "Watching TV ",
+            "Programming "
+        };
+        person.Interests = interests;
+        person.BetterGreeting();
+    }
+}
